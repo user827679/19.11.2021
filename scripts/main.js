2fd1a7196c6e4ed466bd2cdb18417131a4f5d058
Байбачинова Жасмин Талгатовна <93739773+user827679@users.noreply.github.com>
2021-11-19 14:06:30 +0600

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,24 @@
+
+		
+	    function test() {
+			let name = prompt("Имя")
+			document.getElementById("name").textContent = "Имя:" + name
+			
+			let age = prompt("Возраст")
+			document.getElementById("age").textContent = "Возраст:" + age
+			
+			let fam = prompt("Фамилия")
+			document.getElementById("fam"). textContent = "Фамилия:" + fam 
+			
+			let prof = prompt("Спец.ть")
+			document.getElementById("prof"). textContent = "Спец.ть:" + prof
+			
+			let data = prompt("Дата рождения")
+			document.getElementById("data"). textContent ="Дата рождения:" + data 
+			
+			let me = prompt("О себе")
+			document.getElementById("me"). textContent ="О себе:" + me
+		}
+		
+	
+	
